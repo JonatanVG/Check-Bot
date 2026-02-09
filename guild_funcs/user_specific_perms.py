3a5202378f2429afcb2a1bacaf6590167a73c038
JonatanVG <jonatangrondahl@outlook.com>
2026-02-09 11:30:16 +0100

    Main 2.0

--- guild_funcs/user_specific_perms.py
+++ guild_funcs/user_specific_perms.py
@@ -4,33 +4,32 @@ from discord import app_commands
 # This function checks if a user has specific permissions in the database. 
 # It returns True if the user has permissions, and False otherwise.
 def user_specific_perms(supabase):
-    async def predicate(obj):
-        if isinstance(obj, commands.Context):
-            user_id = obj.author.id
-            print(f"Checking permissions for user ID: {user_id} in command context.")
-        elif isinstance(obj, discord.Interaction):
-            user_id = obj.user.id
-            print(f"Checking permissions for user ID: {user_id} in interaction context.")
-        else:
-            return False  # If it's neither, deny access
-        # Query the database for the user's permissions
+    async def slash_predicate(inter: discord.Interaction) -> bool:
+        user_id = inter.user.id
         response = (
-            supabase
-            .table("AUTHORIZED_BOT_USERS")
+            supabase.table("AUTHORIZED_BOT_USERS")
             .select("*")
             .eq("USER_ID", user_id)
             .execute()
         )
-        data = response.data
-        if bool(data):
-            # Check if the user has the required permissions
-            print(f"User ID: {user_id} - Permissions: {data}")
-            return bool(data)  # Return True if permissions exist, False otherwise
-        return False
+        await inter.response.defer() # Defer the response to give the bot more time to check the database.
+        if bool(response.data) == False:
+            await inter.followup.send("You do not have permission to use this command. Please contact the bot administrator for more information.")
+        return bool(response.data)
+    async def prefix_predicate(ctx: commands.Context) -> bool:
+        user_id = ctx.author.id
+        response = (
+            supabase.table("AUTHORIZED_BOT_USERS")
+            .select("*")
+            .eq("USER_ID", user_id)
+            .execute()
+        )
+        await ctx.defer() # Defer the response to give the bot more time to check the database.
+        if bool(response.data) == False:
+            await ctx.send("You do not have permission to use this command. Please contact the bot administrator for more information.")
+        return bool(response.data)
     def decorator(func):
-        print(f"Applying user_specific_perms decorator to {func.__name__}")
-        func = commands.check(predicate)(func)
-        func = app_commands.check(predicate)(func)
+        func = app_commands.check(slash_predicate)(func)
+        func = commands.check(prefix_predicate)(func)
         return func
-    
     return decorator
